e579afc29d951d112076f4c4b313feedfb2fc8ee
yintian <yintian710@gmail.com>
2025-10-23 09:53:59 +0800

    feat(arq): 支持任务结果异步获取- 在 Task 类中新增 result 属性用于存储异步结果 - 新增 ensure 方法订阅结果队列并启动监听 - 新增 get_result 方法支持超时等待任务结果 - 修改 put_task 方法支持自动调用 ensure - 调整 Task 初始化参数增加 result_queue 字段-优化任务投放逻辑以支持新功能集成

--- ytools/arq/client/client.py
+++ ytools/arq/client/client.py
@@ -18,22 +18,24 @@ class Client(BaseClient):
 
     async def put(self, data, task_id=None, **kwargs):
         task_id = task_id or str(uuid4())
-        task = Task(data, self, task_id, **kwargs)
-        await self.put_task(task)
+        task = Task(data=data, client=self, task_id=task_id, result_queue=self.get_queue(task_id, base=self.result_queue), **kwargs)
+        await self.put_task(task, kwargs.get('auto_ensure', False))
         return task
 
-    async def put_task(self, task: Task):
+    async def put_task(self, task: Task, auto_ensure=False):
         data_queue = self.get_queue(task.task_id, base=self.data_queue)
 
         async with self.redis.pipeline(transaction=True) as pipe:
+            auto_ensure and await task.ensure()
             await pipe.zadd(self.tasks_queue, {task.task_id: task.score})
             await pipe.set(data_queue, task.encode_data(), ex=setting.EXPIRE_TIME)
             results = await pipe.execute()
             if not all(results):  # 检查是否有命令失败
                 raise ValueError(f"投放任务至队列失败: {results}")
 
-    async def result(self, task, timeout=None):
-        res = await self.get_result_by_id(task.task_id, timeout=timeout)
+    @staticmethod
+    async def get_result(task: Task, timeout=None):
+        res = await task.get_result(timeout=timeout)
         return res
 
     async def get_result_by_id(self, task_id, timeout=None):
--- ytools/arq/task/task.py
+++ ytools/arq/task/task.py
@@ -6,7 +6,9 @@
 @Software: PyCharm
 @Desc    : 
 """
+import asyncio
 import json
+from asyncio import Future
 from itertools import count
 from uuid import uuid4
 
@@ -17,13 +19,15 @@ from ytools.utils.encrypt import SaltBase64
 
 class Task:
     _score = count()
+    result: Future | None = None
 
-    def __init__(self, data, client, task_id=None, fmt=False, **kwargs):
+    def __init__(self, data, client, task_id=None, result_queue="", fmt=False, **kwargs):
         if fmt and setting.OBJ_DATA:
             data = magic.json_or_eval(data)
         self.data = data
         self.task_id = task_id or str(uuid4())
         self.client = client
+        self.result_queue = result_queue
         self.score = kwargs.get('score') or next(Task._score)
 
     def encode_data(self, data=None):
@@ -58,6 +62,30 @@ class Task:
         if setting.OBJ_DATA:
             data = magic.json_or_eval(data.decode(setting.DEFAULT_ENCODING))
         return data
+
+    async def ensure(self):
+        pubsub = self.client.pubsub()
+        await pubsub.subscribe(self.result_queue)
+
+        async def get_result():
+            try:
+                async for msg in pubsub.listen():
+                    if msg["type"] == "message":
+                        return msg["data"]
+            finally:
+                await pubsub.unsubscribe(self.result_queue)
+                await pubsub.close()
+
+        self.result = asyncio.ensure_future(get_result())
+
+    async def get_result(self, timeout=None):
+        if not self.result or self.result.cancelled():
+            raise RuntimeError("未启动 ensure, 无法获取结果")
+        try:
+            result = await asyncio.wait_for(self.result, timeout=timeout)
+        finally:
+            self.result.cancel()
+        return result
 
 
 if __name__ == '__main__':
