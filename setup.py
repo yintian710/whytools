0a0fe18265e24324e3cc923cdf7422b992c92b2c
yintian <yintian710@gmail.com>
2025-11-04 16:52:28 +0800

    v0.2.0

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ AUTHOR = 'yintian'
 REQUIRES_PYTHON = '>=3.8.0'
 logger.error(f"!!!!!!")
 if len(sys.argv) == 1:
-    VERSION = update_version()
+    VERSION = update_version(update_type="minor")
 else:
     VERSION = get_version()
 
