7bc6e4937f3fa9b97b8b56594907165c459e45bf
yintian <yintian710@gmail.com>
2025-11-04 14:07:31 +0800

    feat(arq): 增强任务状态管理功能
    - 修改 set_status 方法，支持在数据不存在时不执行 redis 的 set 操作
    - 在 set_status 方法中增加对已存在键的过期时间更新逻辑
    - 新增 del_status 方法，用于删除指定的状态键
    - 调整 set_status 和 get_status 方法中的队列名称处理逻辑

--- ytools/arq/task/task.py
+++ ytools/arq/task/task.py
@@ -87,13 +87,21 @@ class Task:
             self.result.cancel()
         return result
 
-    async def set_status(self, data, status_name="status", expire_time=None):
+    async def set_status(self, data=None, status_name="status", expire_time=None):
         status_queue = self.client.get_queue(status_name, base=self.result_queue)
-        await self.client.redis.set(status_queue, data, ex=expire_time or setting.EXPIRE_TIME)
+        expire_time = expire_time or setting.EXPIRE_TIME
+        if data is not None:
+            await self.client.redis.set(status_queue, data, ex=expire_time)
+        if await self.client.redis.exists(status_name):
+            await self.client.redis.expire(status_queue, expire_time)
 
     async def get_status(self, status_name="status"):
         status_queue = self.client.get_queue(status_name, base=self.result_queue)
         return await self.client.redis.get(status_queue)
+
+    async def del_status(self, status_name="status"):
+        status_queue = self.client.get_queue(status_name, base=self.result_queue)
+        return await self.client.redis.delete(status_queue)
 
 
 if __name__ == '__main__':
